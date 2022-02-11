4ce3f9a49821f106a0bbfd8582fd868c3ec94cda
MBA <mbasonny@gmail.com>
2022-02-11 22:20:09 +0100

    Access

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -714,7 +714,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
             }            
         }
         if(!in_array($droit, $droits)){
-            //return "d-none";
+            return "d-none";
         }
         return;
     }
