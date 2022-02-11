17001b5ba3bb1ffbcd5489b0f993474b03924246
landry <skt.dev.landry@gmail.com>
2022-02-11 11:42:18 +0100

    controller du client et compte

--- src/Controller/DashboadController.php
+++ src/Controller/DashboadController.php
@@ -25,37 +25,42 @@ class DashboadController extends DefaultController
     {
         
         $link="index-dashboad";
-        try {
-			if($this->getUser()->getIsadmin()){
-				$entene = $this->em->getRepository(Entene::class)->findAll();
-            	$typec = $this->em->getRepository(Typechambre::class)->findAll();
-            	$chambre = $this->em->getRepository(Chambre::class)->findAll();
-			}else{
-				$entene = $this->em->getRepository(Entene::class)->findBy([
-					'id' => $this->getUser()->getAntene()->getId()
-				]);
-            	$typec = $this->em->getRepository(Typechambre::class)->findBy([
-					'antene'=> $this->getUser()->getAntene()
-				]);
-            	$chambre = $this->em->getRepository(Chambre::class)->findBy([
-					'entene'=> $this->getUser()->getAntene()
-				]);
-			}
-            
-            $nbentene = count($entene);
-            $nbchambre = count($chambre);
+        $userg = $this->getUser();
+        if($userg){
+            try {
+                if($this->getUser()->getIsadmin()){
+                    $entene = $this->em->getRepository(Entene::class)->findAll();
+                    $typec = $this->em->getRepository(Typechambre::class)->findAll();
+                    $chambre = $this->em->getRepository(Chambre::class)->findAll();
+                }else{
+                    $entene = $this->em->getRepository(Entene::class)->findBy([
+                        'id' => $this->getUser()->getAntene()->getId()
+                    ]);
+                    $typec = $this->em->getRepository(Typechambre::class)->findBy([
+                        'antene'=> $this->getUser()->getAntene()
+                    ]);
+                    $chambre = $this->em->getRepository(Chambre::class)->findBy([
+                        'entene'=> $this->getUser()->getAntene()
+                    ]);
+                }
+                
+                $nbentene = count($entene);
+                $nbchambre = count($chambre);
 
-            $data = $this->renderView('admin/dashboad/index.html.twig', [
-                "nbentene" => $nbentene,
-                "nbchambre" => $nbchambre,
-                "entenes" => $entene,
-                "typecs" => $typec,
-            ]);
-            $this->successResponse("Dahboard", $link, $data);
+                $data = $this->renderView('admin/dashboad/index.html.twig', [
+                    "nbentene" => $nbentene,
+                    "nbchambre" => $nbchambre,
+                    "entenes" => $entene,
+                    "typecs" => $typec,
+                ]);
+                $this->successResponse("Dahboard", $link, $data);
 
-        } catch (\Exception $ex) {
-            $this->log($ex->getMessage(), $link);
+            } catch (\Exception $ex) {
+                $this->log($ex->getMessage(), $link);
+            }
+            return $this->json($this->result);
+        }else{
+            return $this->redirectToRoute('login');
         }
-        return $this->json($this->result);
     }
 }
